4ebe4ecd5184486738e42f599e310b05c08d0a25
miskaldukhi <miskaldukhi@gmail.com>
2025-02-17 19:40:41 +0300

    companion done

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,26 @@
 fun main() {
-    println("Hello World!")
+println("40.5 Degrees Celsius = ${TemperatureConverter.celsiusToFahrenheit(40.5)} Degrees Fahrenheit")
+    println("74.5 Degrees Fahrenheit = ${TemperatureConverter.fahrenheitToCelsius(74.5)} Degrees Celsius")
+    println("74.5 Degrees Fahrenheit = ${TemperatureConverter.fahrenheitToKelvin(74.5)} Kelvin")
+    println("40.5 Degrees Celsius = ${TemperatureConverter.celsiusToKelvin(40.5)} Kelvin")
+
+    }
+
+class TemperatureConverter {
+    companion object {
+
+        fun celsiusToFahrenheit(celsius: Double): Double {
+            return 9 / 5 * (celsius) + 32
+        }
+
+        fun fahrenheitToCelsius(fahrenheit: Double): Double {
+            return (fahrenheit - 32) * 5 / 9
+        }
+        fun celsiusToKelvin(celsius: Double): Double {
+            return celsius + 273
+        }
+        fun fahrenheitToKelvin(fahrenheit: Double): Double {
+            return fahrenheit - 273
+        }
+    }
 }
